a94281b007a1512a7cfaedce9199905ea0976d6a
Matheus Calegaro <hello@matheus.me>
2021-09-24 01:01:12 -0300

    feat(pages/index): adding form and url validation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,60 @@
+import { useState, MouseEvent } from 'react';
+
 const IndexPage: React.FC = () => {
+  const [url, setUrl] = useState<string>('');
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onSubmitPress = async (e: MouseEvent<HTMLElement>): Promise<void> => {
+    e.preventDefault();
+    setError(null);
+
+    // FIXME: move validation to its own service
+    const exp = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g;
+
+    if (url === '' || !exp.test(url)) {
+      setError('Invalid URL!');
+      return;
+    }
+
+    setIsLoading(false);
+
+    // FIXME: move this to its own service
+    try {
+      // TODO: call API route
+    } catch (err) {
+      console.error(err);
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div>
       <h1>Link shortener</h1>
+
+      {error && (
+        <div className="error">
+          <p>{error}</p>
+        </div>
+      )}
+
+      <form name="urlShortener">
+        <label htmlFor="url">URL</label>
+        <input
+          type="text"
+          name="url"
+          onChange={(e) => setUrl(e.target.value)}
+          disabled={isLoading}
+        />
+
+        <button type="button" onClick={!isLoading && onSubmitPress} disabled={isLoading}>
+          Make it short!
+        </button>
+      </form>
     </div>
-  )
+  );
 };
 
 export default IndexPage;
