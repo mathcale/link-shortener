7d3f4314a5d6c36cb2c1bae2c796c1d186c8fe85
Matheus Calegaro <hello@matheus.me>
2021-09-27 15:18:58 -0300

    chore(tailwind): passing Alert dynamic classes to purge safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+    safelist: [
+      'bg-green-100',
+      'bg-green-200',
+      'bg-red-100',
+      'bg-red-200',
+      'border-green-300',
+      'border-green-500',
+      'border-red-300',
+      'border-red-500',
+      'text-green-500',
+      'text-green-600',
+      'text-green-800',
+      'text-red-500',
+      'text-red-600',
+      'text-red-800',
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
