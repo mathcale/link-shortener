8430a8753697ee6c40381a13cd3f4f0b98c95bd0
Freddy González <7684330+fredoist@users.noreply.github.com>
2021-10-12 22:27:11 -0500

    feat: implement dark theme based on `prefers-color-scheme`

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
       'text-red-800',
     ],
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
