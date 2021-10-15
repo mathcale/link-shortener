4b092955c6aed458faee006fd18b23fadea0ee16
Allwell Onen <aleenfestus@gmail.com>
2021-10-15 22:02:03 +0100

    Implement dark mode feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
-import { useState, MouseEvent } from 'react';
-import Head from 'next/head';
-import ReactTooltip from 'react-tooltip';
+import { MouseEvent, useState } from 'react';
 
 import { Alert } from '../components';
+import Head from 'next/head';
+import Image from 'next/image';
+import ReactTooltip from 'react-tooltip';
 import { UrlService } from '../services';
+import toggleOff from '../../public/toggle-off.png';
+import toggleOn from '../../public/toggle-on.png';
 
 const IndexPage: React.FC = () => {
   const INITIAL_TOOLTIP_TEXT = 'Copy to clipboard';
+
+  const [isDark, setIsDark] = useState<boolean>(false);
 
   const [url, setUrl] = useState<string>('');
   const [error, setError] = useState<string | null>(null);
@@ -71,65 +76,88 @@ const IndexPage: React.FC = () => {
       <Head>
         <title>MiniLinks - a simple link shortener by @mathcale</title>
       </Head>
+      <main className={isDark ? 'dark' : ''}>
+        <div className="bg-gray-200 pb-10 pt-32 px-10 md:px-0 flex flex-col justify-between h-screen dark:bg-gray-900">
+          <div>
+            <h1 className="text-center text-4xl font-bold mb-2 dark:text-gray-200">
+              Link Shortener
+            </h1>
+            <p className="text-center text-gray-500">It shortens your links. That&apos;s it 🙃</p>
 
-      <main className="pt-20 px-10 md:px-0 flex flex-col justify-between h-screen">
-        <div>
-          <h1 className="text-center text-4xl font-bold mb-2 dark:text-white">Link Shortener</h1>
-          <p className="text-center text-gray-600 dark:text-gray-400">It shortens your links. That&apos;s it 🙃</p>
+            <div className="flex items-center w-full mt-10">
+              <div className="w-full md:max-w-2xl md:mx-auto">
+                {error && <Alert type="error" title="Error" message={error} />}
 
-          <div className="flex items-center w-full mt-10">
-            <div className="w-full md:max-w-2xl md:mx-auto">
-              {error && <Alert type="error" title="Error" message={error} />}
+                {shorterUrl && (
+                  <Alert type="success" title="Success" CustomMessage={renderSuccessAlertMessage} />
+                )}
 
-              {shorterUrl && (
-                <Alert type="success" title="Success" CustomMessage={renderSuccessAlertMessage} />
-              )}
+                <form className="md:flex md:flex-wrap md:justify-between">
+                  <div className="flex flex-col mb-4 md:w-3/4">
+                    <input
+                      type="text"
+                      name="url"
+                      placeholder="Type your URL here..."
+                      onChange={(e) => setUrl(e.target.value)}
+                      disabled={isLoading}
+                      className="bg-white md:mr-5 p-4 shadow rounded-lg focus:outline-none focus:ring focus:border-blue-700"
+                    />
+                  </div>
 
-              <form className="md:flex md:flex-wrap md:justify-between">
-                <div className="flex flex-col mb-4 md:w-3/4">
-                  <input
-                    type="text"
-                    name="url"
-                    placeholder="Type your URL here..."
-                    onChange={(e) => setUrl(e.target.value)}
-                    disabled={isLoading}
-                    className="bg-white md:mr-5 p-4 shadow rounded-lg focus:outline-none focus:ring focus:ring-opacity-20 focus:ring-blue-700 dark:bg-gray-800 dark:text-white dark:focus:ring-gray-200"
-                  />
-                </div>
-
-                <div className="flex flex-col mb-4 md:w-1/4">
-                  <button
-                    type="button"
-                    onClick={(e) => (!isLoading ? onSubmitPress(e) : null)}
-                    disabled={isLoading}
-                    className="p-4 rounded-lg bg-blue-600 hover:bg-blue-700 text-white transition-colors dark:bg-blue-800 dark:hover:bg-blue-600"
-                  >
-                    Make it short!
-                  </button>
-                </div>
-              </form>
+                  <div className="flex flex-col mb-4 md:w-1/4">
+                    <button
+                      type="button"
+                      onClick={(e) => (!isLoading ? onSubmitPress(e) : null)}
+                      disabled={isLoading}
+                      className="p-4 rounded-lg bg-blue-500 hover:bg-blue-700 text-white transition-colors"
+                    >
+                      Make it short!
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
 
-        <p className="mb-5 text-gray-600 text-center text-xs dark:text-gray-400">
-          Created with{' '}
-          <a href="https://nextjs.org/" target="_blank" rel="noreferrer" className="underline">
-            Next.js
-          </a>
-          ,{' '}
-          <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer" className="underline">
-            Tailwind
-          </a>{' '}
-          and{' '}
-          <a href="https://upstash.com/" target="_blank" rel="noreferrer" className="underline">
-            Upstash
-          </a>{' '}
-          by{' '}
-          <a href="https://matheus.me" target="_blank" rel="noreferrer" className="underline">
-            Matheus Calegaro
-          </a>
-        </p>
+          <div>
+            {/* TOGGLE MODE */}
+            <div className="flex justify-center text-sm items-center text-center text-gray-500 dark:text-gray-200">
+              <span className={isDark ? 'font-thin' : 'text-blue-600 font-bold'}>Light</span>
+              <button
+                className="mx-2"
+                onClick={() => (isDark ? setIsDark(false) : setIsDark(true))}
+              >
+                <Image src={isDark ? toggleOn : toggleOff} width={50} height={50} alt="toggle" />
+              </button>
+              <span className={isDark ? 'font-bold text-blue-600' : 'font-thin'}>Dark</span>
+            </div>
+            {/* END - TOGGLE MODE */}
+
+            <p className="mb-5 text-gray-400 text-center text-xs">
+              Created with{' '}
+              <a href="https://nextjs.org/" target="_blank" rel="noreferrer" className="underline">
+                Next.js
+              </a>
+              ,{' '}
+              <a
+                href="https://tailwindcss.com/"
+                target="_blank"
+                rel="noreferrer"
+                className="underline"
+              >
+                Tailwind
+              </a>{' '}
+              and{' '}
+              <a href="https://upstash.com/" target="_blank" rel="noreferrer" className="underline">
+                Upstash
+              </a>{' '}
+              by{' '}
+              <a href="https://matheus.me" target="_blank" rel="noreferrer" className="underline">
+                Matheus Calegaro
+              </a>
+            </p>
+          </div>
+        </div>
       </main>
     </>
   );
